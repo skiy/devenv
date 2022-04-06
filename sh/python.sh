54aa71336b6b53cd756e7a8b6320c2cbb5ba2f9d
Jetsung Chan <skiy@jetsung.com>
2022-04-07 01:33:14 +0800

    update: python pip install

--- sh/python.sh
+++ sh/python.sh
@@ -13,10 +13,14 @@ main() {
 
     check_in_china
 
+    if !(test -x "$(command -v pip)"); then
+        curl -fSL https://bootstrap.pypa.io/get-pip.py | sudo python
+    fi
+
     if [ "${IN_CHINA}" == "1" ]; then
         # pip install pip -U
-        pip install -i https://pypi.tuna.tsinghua.edu.cn/simple pip -U
-        pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
+        pip install -i https://pypi.tuna.tsinghua.edu.cn/simple pip -U  
+        pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple --trusted-host pypi.tuna.tsinghua.edu.cn
     fi    
 }
 
